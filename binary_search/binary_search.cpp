337161b89fb85429102c03c746ad47d9798b0ea8
swjung <121101975+jswnote@users.noreply.github.com>
2025-12-25 01:31:48 +0900

    Create binary_search.cpp

---
+++ binary_search/binary_search.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+template <typename T>
+int binary_search(vector<T>& arr, int start, int end, int target)
+{
+    int mid = (start + end) / 2;
+    int target_idx;
+
+    if(arr[mid] < target)
+    {
+        binary_search(arr, start, mid, target);
+    }
+
+    else if (arr[mid] > target)
+    {
+        binary_search(arr, mid+1, end, target);
+    }
+    else if (arr[mid] == target)
+    {
+        return mid;
+    }
+}
