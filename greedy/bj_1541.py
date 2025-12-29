e5d02b556fa754f62c5a37061d26338056ec6868
swjung <121101975+jswnote@users.noreply.github.com>
2025-12-29 16:09:26 +0900

    Update bj_1541.py

--- greedy/bj_1541.py
+++ greedy/bj_1541.py
@@ -1,1 +1,10 @@
+#잃어버린 괄호
 
+N = [x for x in input().split(sep='-')]
+
+result = sum(map(int, N[0].split(sep="+")))
+
+for x in N[1:]:
+    result -= sum(map(int, x.split(sep="+")))
+
+print(result)
