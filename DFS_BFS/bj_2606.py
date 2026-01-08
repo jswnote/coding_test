2f9dbbb86bbde641ae742744f20021d4af37a59c
swjung <121101975+jswnote@users.noreply.github.com>
2026-01-08 17:04:43 +0900

    Create bj_2606.py

---
+++ DFS_BFS/bj_2606.py
@@ -0,0 +1,25 @@
+import sys
+
+def dfs(graph, visited, v):
+    visited[v] = True
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, visited, i)    
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(len(graph)):
+    graph[i].sort()
+
+dfs(graph, visited, 1)
+
+print(sum(visited) - 1)
