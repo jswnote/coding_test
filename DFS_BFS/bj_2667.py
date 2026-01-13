00a4ede8435156e76447a1df76ec83951f97cd4c
swjung <121101975+jswnote@users.noreply.github.com>
2026-01-13 17:25:34 +0900

    Update bj_2667.py

--- DFS_BFS/bj_2667.py
+++ DFS_BFS/bj_2667.py
@@ -1,36 +1,77 @@
-#단지번호
+#단지 번호
+from collections import deque
 import sys
 
-def dfs(x, y, N, grid):
+# def bfs(graph, x, y, visited, N):
+#     if x < 0 or x >= N or y < 0 or y >= N:
+#         return 0
+#     visited[x][y] = True
+#     count = 1
+    
+#     dx = [-1, 1, 0, 0]
+#     dy = [0, 0, -1, 1]
+    
+#     queue = deque([(x,y)])
+    
+#     while queue:
+#         cx, cy = queue.popleft()
+        
+#         for i in range(4):
+#             nx, ny = cx + dx[i], cy + dy[i]
+#             if 0 <= nx < N and 0 <= ny < N:
+#                 if graph[nx][ny] == 1 and not visited[nx][ny]:
+#                     queue.append((nx, ny))
+#                     visited[nx][ny] = True
+#                     count += 1
+    
+#     return count
+
+
+# N = int(input())
+# graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
+# visited = [[False] * N for _ in range(N)]
+# part_count = []
+
+# for i in range(N):
+#     for j in range(N):
+#         if graph[i][j] == 1 and not visited[i][j]:
+#             part_count.append(bfs(graph, i, j, visited, N))
+
+# part_count.sort()
+# print(len(part_count))
+# for c in part_count:
+#     print(c)
+    
+sys.setrecursionlimit(10**6) #재귀 깊이 제한
+
+def dfs(graph, x, y, visited, N):
     if x < 0 or x >= N or y < 0 or y >= N:
         return 0
+    visited[x][y] = True
+    count = 1
     
-    if grid[x][y] == 1:
-        grid[x][y] = 0
-        count = 1
-        #bible
-        dx = [-1,1,0,0]
-        dy = [0,0,-1,1]
-
-        for i in range(4):
-            count += dfs(x+dx[i],y+dy[i], n, grid)
-        return count
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
-    return 0
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                count += dfs(graph, nx, ny, visited, N)
+    
+    return count
 
-n = int(input())
-#bible
-grid_map = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+N = int(input())
+graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+part_count = []
 
-result = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1 and not visited[i][j]:
+            part_count.append(dfs(graph, i, j, visited, N))
 
-for i in range(n):
-    for j in range(n):
-        size = dfs(i,j,n,grid_map)
-        if size > 0:
-            result.append(size)
-            
-result.sort()
-print(len(result))
-for r in result:
-    print(r)
+part_count.sort()
+print(len(part_count))
+for c in part_count:
+    print(c)
